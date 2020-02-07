fe2c0c394096757e0d8495ff3b905c8d48740230
eleni <eleni.aslanidou@student.kyh.se>
2020-02-07 11:55:07 +0100

    cahnges in App.js

--- src/App.js
+++ src/App.js
@@ -4,41 +4,101 @@ import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
 
-class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div className="demo-big-content">
-            <Layout>
-              <Header className='header-color' title={<Link style={{ textDecoration: 'none', color: 'white' }} to="/">My Portfolio</Link>} scroll>
-                <Navigation>
-                  <Link to="/Arbetslivserfarenhet" style={{fontSize: '20px', color: 'white', fontFamily: 'Dancing Script' }}>Arbetslivserfarenhet</Link>
-                  <Link to="/UtbildningDatakunskaper" style={{fontSize: '20px', color: 'white', fontFamily: 'Dancing Script' }}>Utbildning-Datakunskaper</Link>
-                  <Link to="/projects" style={{fontSize: '20px', color: 'white', fontFamily: 'Dancing Script' }}>Projekt</Link>
-                  <Link to="/contact" style={{fontSize: '20px', color: 'white', fontFamily: 'Dancing Script' }}>Kontakt</Link>
-                </Navigation>
-              </Header>
+import Button from './Button'; // Import a component from another file
 
-    <Drawer title={<Link style={{textDecoration: 'none', color: 'blue', fontSize: '25px', fontFamily: 'Dancing Script'}} to="/">Home</Link>} >
-                  <Navigation>
-                    <Link to="/Arbetslivserfarenhet" style={{color: 'black', fontSize: '25px', fontFamily: 'Dancing Script'}}>Arbetslivserfarenhet</Link>
-                    <Link to="/UtbildningDatakunskaper" style={{color: 'black', fontSize: '25px', fontFamily: 'Dancing Script'}}>Utbildning-Datakunskaper</Link>
-                    <Link to="/projects" style={{color: 'black', fontSize: '25px', fontFamily: 'Dancing Script'}}>Projekt</Link>
-                    <Link to="/contact" style={{color: 'black', fontSize: '25px', fontFamily: 'Dancing Script'}}s>Kontakt</Link>
-                  </Navigation>
-    </Drawer>
 
-              <Content>
-                <div className="page-content" />
-                <Main/>
-              </Content>
-            </Layout>
-          </div>
-        </header>
-      </div>
-    );
+const list = [
+  {
+    title: 'React1',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, {
+    title: 'Redux2',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },];
+
+function isSearched(searchTerm) {
+  return function (item) {
+    return !searchTerm ||
+      item.title.toLowerCase().includes(searchTerm.toLowerCase());
   }
 }
+
+// API KEY: APPID=f8384513fad5f91ea04d07a2cbf916ec
+const API = "http://api.openweathermap.org/data/2.5/weather?q=stockholm,se&APPID=f8384513fad5f91ea04d07a2cbf916ec&units=metric";
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: ''
+    };
+    
+  }
+  componentDidMount() {
+    console.log("CDM ran")
+    fetch(API)
+      .then(response => response.json())
+      .then(data => this.setState({ weather: data.main.feels_like }));
+  }
+  
+  render() {
+    //const {  weather } = this.state;
+    const weather = this.state.weather;
+    return (
+
+      <div className="App">
+      <header className="App-header">
+        <div className="demo-big-content">
+          <Layout>
+            <Header className='header-color' title={<Link style={{ textDecoration: 'none', color: 'white' }} to="/">My Portfolio</Link>} scroll>
+              <Navigation>
+                <Link to="/Arbetslivserfarenhet" style={{fontSize: '20px', color: 'white', fontFamily: 'Dancing Script' }}>Arbetslivserfarenhet</Link>
+                <Link to="/UtbildningDatakunskaper" style={{fontSize: '20px', color: 'white', fontFamily: 'Dancing Script' }}>Utbildning-Datakunskaper</Link>
+                <Link to="/projects" style={{fontSize: '20px', color: 'white', fontFamily: 'Dancing Script' }}>Projekt</Link>
+                <Link to="/contact" style={{fontSize: '20px', color: 'white', fontFamily: 'Dancing Script' }}>Kontakt</Link>
+              </Navigation>
+            </Header>
+
+  <Drawer title={<Link style={{textDecoration: 'none', color: 'blue', fontSize: '25px', fontFamily: 'Dancing Script'}} to="/">Home</Link>} >
+                <Navigation>
+                  <Link to="/Arbetslivserfarenhet" style={{color: 'black', fontSize: '25px', fontFamily: 'Dancing Script'}}>Arbetslivserfarenhet</Link>
+                  <Link to="/UtbildningDatakunskaper" style={{color: 'black', fontSize: '25px', fontFamily: 'Dancing Script'}}>Utbildning-Datakunskaper</Link>
+                  <Link to="/projects" style={{color: 'black', fontSize: '25px', fontFamily: 'Dancing Script'}}>Projekt</Link>
+                  <Link to="/contact" style={{color: 'black', fontSize: '25px', fontFamily: 'Dancing Script'}}s>Kontakt</Link>
+                </Navigation>
+  </Drawer>
+            <Button />
+            <Content>
+
+            <div>The temperature right now feels like {weather || '?'}</div>
+              <div className="page-content" />
+
+     
+    }
+  }
+             
+              <Main/>
+            </Content>
+          </Layout>
+        </div>
+      </header>
+    </div>
+    
+  );
+}
+}
+     
+       
+       
 
 export default App;
