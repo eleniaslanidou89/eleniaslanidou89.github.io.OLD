0455298656a7fb30a561754f1ab56fb6adc763bb
Eleni <57090719+eleniaslanidou89@users.noreply.github.com>
2021-08-16 12:41:38 +0200

    changed link

--- src/components/projects.js
+++ src/components/projects.js
@@ -28,7 +28,7 @@ class Projects extends Component {
           >
             {' '}
             <a
-              href="http://quire-project.surge.sh/"
+              href="https://elenisblog.surge.sh/"
               rel="noopener noreferrer"
               target="_blank"
             >
@@ -37,25 +37,39 @@ class Projects extends Component {
                   color: '#fff',
                   height: '175px',
                   background:
-                    'url(https://logos.textgiraffe.com/logos/logo-name/Eleni-designstyle-breeze-o.png) center/cover',
+                    'url(https://images.unsplash.com/photo-1607964969051-988be31393fd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=666&q=80png) center/cover',
                   width: '200px',
                   marginLeft: '80px',
                 }}
               ></CardTitle>
             </a>
             <CardText style={{ fontSize: '20px', fontFamily: 'Segoe UI' }}>
-              Quire / website
+              Eleni's Blog
+              <br />
+              <br />
+              React website. I used React Hooks and React Router for this React
+              JS Project. The website is fully responsive as well.
             </CardText>
             <CardActions border>
               {/*link for github*/}
               <a
-                href="https://github.com/eleniaslanidou89/Quire"
+                href="https://github.com/eleniaslanidou89/Eleni-s_Blog/tree/master"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 <Button colored>
                   GitHub{' '}
                   <i className="fab fa-github-square" aria-hidden="true" />
+                </Button>
+              </a>
+              {/*link for the website*/}
+              <a
+                href="https://elenisblog.surge.sh/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <Button colored>
+                  LINK <i class="fas fa-link" aria-hidden="true" />
                 </Button>
               </a>
             </CardActions>
@@ -124,47 +138,6 @@ class Projects extends Component {
               ></CardTitle>
             </a>
             <CardText style={{ fontSize: '20px', fontFamily: 'Segoe UI' }}>
-              Comments / app
-              <br />
-              <br /> Blog
-            </CardText>
-            <CardActions border>
-              {/*link for github*/}
-              <a
-                href="https://github.com/eleniaslanidou89/comments-project"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <Button colored>
-                  GitHub{' '}
-                  <i className="fab fa-github-square" aria-hidden="true" />
-                </Button>
-              </a>
-            </CardActions>
-          </Card>
-
-          {/*Project #4*/}
-          <Card
-            shadow={5}
-            style={{ minWidth: '500', marginTop: '80px', marginLeft: '50px' }}
-          >
-            <a
-              href="http://bookstore-eleni.herokuapp.com/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <CardTitle
-                style={{
-                  color: '#fff',
-                  height: '175px',
-                  background:
-                    'url(https://logos.textgiraffe.com/logos/logo-name/Eleni-designstyle-breeze-o.png) center/cover',
-                  width: '200px',
-                  marginLeft: '80px',
-                }}
-              ></CardTitle>
-            </a>
-            <CardText style={{ fontSize: '20px', fontFamily: 'Segoe UI' }}>
               My store
               <br />
               <br /> Angular application with components
@@ -181,15 +154,14 @@ class Projects extends Component {
                   <i className="fab fa-github-square" aria-hidden="true" />
                 </Button>
               </a>
-                {/*link for app*/}
-                <a
+              {/*link for app*/}
+              <a
                 href="https://angular-fyx8tk-rwft5n.stackblitz.io"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 <Button colored>
-                  App{' '}
-                  <i class="fab fa-angular" aria-hidden="true" />
+                  LINK <i class="fas fa-link" aria-hidden="true" />
                 </Button>
               </a>
             </CardActions>
