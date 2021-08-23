8c593321d5293d1840f711e8445d86f40ddf1e56
Eleni <57090719+eleniaslanidou89@users.noreply.github.com>
2021-08-23 10:37:17 +0200

    updated structure
    deleted the things that didn't used

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,25 +1,5 @@
-/* import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
-import { BrowserRouter } from 'react-router-dom';
-
-ReactDOM.render(
-<BrowserRouter>
-<App />
-</BrowserRouter>
-, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
- */
-
 import React, { Component } from 'react'
-// TOdo. 1. make this code available to APp.js 2. refactor into FSC
+
 class Button extends Component {
   render() {
     const { onClick, className = '', children } = this.props
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Grid, Cell, Layout } from "react-mdl";
+import React, { Component } from 'react'
+import { Grid, Cell, Layout } from 'react-mdl'
 
 class Contact extends Component {
   render() {
@@ -8,20 +8,21 @@ class Contact extends Component {
         <Layout
           style={{
             background:
-              "url(https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80)  center / cover",
+              'url(https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80)  center / cover',
           }}
         >
           <Grid className="contact-grid">
             <Cell className="title" col={6}>
               <h2>Eleni Aslanidou</h2>
               <p>
-                My name is Eleni Aslanidou. I am Front-end Developer. I have bachelor in IT and Communications
-                Engineering. As a person I have great energy and I am open
-                minded. I have the "of course I can" attitude because I am not
-                afraid of new challenges. I always strive to improve myself.
-                Please contact me if you have any questions or concern.
+                My name is Eleni Aslanidou. I am Front-end Developer. I have
+                bachelor in IT and Communications Engineering. As a person I
+                have great energy and I am open minded. I have the "of course I
+                can" attitude because I am not afraid of new challenges. I
+                always strive to improve myself. Please contact me if you have
+                any questions or concern.
               </p>
-              <i className="fa fa-envelope-open" aria-hidden="true" />{" "}
+              <i className="fa fa-envelope-open" aria-hidden="true" />{' '}
               <p className="contactInfo">helena.aslanidou@gmail.com</p>
               <i className="fab fa-linkedin" aria-hidden="true" />
               <p className="contactInfo">
@@ -31,8 +32,8 @@ class Contact extends Component {
           </Grid>
         </Layout>
       </div>
-    );
+    )
   }
 }
 
-export default Contact;
+export default Contact
