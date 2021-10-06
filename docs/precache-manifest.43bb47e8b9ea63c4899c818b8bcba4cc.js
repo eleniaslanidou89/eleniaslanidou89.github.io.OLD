bdba891dd42bf8862a52f5d2357c7b97dfc86658
Eleni <helena.aslanidou@gmail.com>
2021-10-06 21:30:12 +0200

    docs for pages

---
+++ docs/precache-manifest.43bb47e8b9ea63c4899c818b8bcba4cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfde6a72774ada9d03492c35ebc68f72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b989a1f3318134881f78",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "4ba001dd4472104fb031",
+    "url": "/static/css/main.2b16760e.chunk.css"
+  },
+  {
+    "revision": "b989a1f3318134881f78",
+    "url": "/static/js/2.e7378725.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/static/js/2.e7378725.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ba001dd4472104fb031",
+    "url": "/static/js/main.d7e7293b.chunk.js"
+  },
+  {
+    "revision": "a115e4e0014e98da6408",
+    "url": "/static/js/runtime-main.451e9585.js"
+  },
+  {
+    "revision": "5e9401ffa48803eefcb04a15dc776cd9",
+    "url": "/static/media/backgroundImage.5e9401ff.jpg"
+  }
+]);
